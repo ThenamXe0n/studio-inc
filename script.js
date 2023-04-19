9f503b254dd3c96f23b4f9002a1db0867a03ec1a
ThenamXe0n <123178362+ThenamXe0n@users.noreply.github.com>
2023-04-19 13:41:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-window.addEventListener('scroll',()=>{
+
+window.addEventListener('scroll', () => {
+    let content = document.querySelector('.landing-page')
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if (contentPosition < screenPosition) {
+        content.classList.add('landing-pageHide');
+    } else {
+        return
+    }
+},)
+
+window.addEventListener('scroll', () => {
+
     let content = document.querySelector('.section')
-    let contentPosition =content.getBoundingClientRect().top;
-    let screenPosition =window.innerHeight;
-    if(contentPosition < screenPosition){
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if (contentPosition < screenPosition) {
         content.classList.add('active');
-    }else{
+    } else {
+        content.classList.remove('active')
+    }
+
+
+})
+
+window.addEventListener('scroll', () => {
+    let content = document.querySelector('.section1')
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if (contentPosition < screenPosition) {
+        content.classList.add('active');
+    } else {
+        content.classList.remove('active')
+    }
+})
+window.addEventListener('scroll', () => {
+    let content = document.querySelector('.section2')
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if (contentPosition < screenPosition) {
+        content.classList.add('active');
+    } else {
         content.classList.remove('active')
     }
 })
 
-window.addEventListener('scroll',()=>{
-    let content = document.querySelector('.section1')
-    let contentPosition =content.getBoundingClientRect().top;
-    let screenPosition =window.innerHeight;
-    if(contentPosition < screenPosition){
-        content.classList.add('active');
-    }else{
-        content.classList.remove('active')
-    }
-})
-window.addEventListener('scroll',()=>{
-    let content = document.querySelector('.section2')
-    let contentPosition =content.getBoundingClientRect().top;
-    let screenPosition =window.innerHeight;
-    if(contentPosition < screenPosition){
-        content.classList.add('active');
-    }else{
-        content.classList.remove('active')
-    }
-})
+
+
