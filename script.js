6cbb916ff3fceb6612262f28f0deca1ed06f7040
ThenamXe0n <123178362+ThenamXe0n@users.noreply.github.com>
2023-04-22 14:20:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 
+        var MenuItems = document.getElementById('MenuItems');
+
+        MenuItems.style.maxHeight = "0px";
+
+        function menutoggle() {
+            if (MenuItems.style.maxHeight == "0px") {
+                MenuItems.style.maxHeight = "800px";
+            }
+            else {
+                MenuItems.style.maxHeight = "0px";
+            }
+        }
+
+
 window.addEventListener('scroll', () => {
     let content = document.querySelector('.landing-page')
     let contentPosition = content.getBoundingClientRect().top;
